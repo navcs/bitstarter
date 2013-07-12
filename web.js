1696191b5321d9d1c3b7fbd9ce9269104bdf5620
Navtez Singh <navtesingh@gmail.com>
2013-07-12 04:12:17 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -3,7 +3,10 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var filebuff = fs.readFileSync('index.html','ascii');
+  var buff = new Buffer(filebuff.length);
+  buff.write(filebuff, 0, filebuff.length);
+  response.send(buff.toString());
 });
 
 var port = process.env.PORT || 5000;
